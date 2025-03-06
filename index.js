915cb8660b518b2a634dfbb2b6567c628efec4aa
Akshatt10 <akshattyagi1010@gmail.com>
2025-03-06 09:54:19 +0530

    vercel deploy

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
-const NGROK_API_URL = 'https://2f70-116-68-245-229.ngrok-free.app/api';
+const NGROK_API_URL = 'https://d3f1-116-68-245-229.ngrok-free.app/api';
 
 // Debugging middleware
 app.use((req, res, next) => {
