48ad4f234f4dff6a09d8ddcd85c42f8cc3736276
Akshatt10 <akshattyagi1010@gmail.com>
2025-04-02 09:16:33 +0530

    ngrok change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const NGROK_API_URL = 'https://8bcd-116-68-245-229.ngrok-free.app/api';
+const NGROK_API_URL = 'https://3798-116-68-245-229.ngrok-free.app/api';
 
 app.use((req, res, next) => {
     console.log(`Received request: ${req.method} ${req.originalUrl}`);
